22296a1834cc8a7540af4f29c3cd77e2dbe1fe8d
tranvansong <songtran02@gmail.com>
2024-12-26 16:11:55 +0700

    init comit

--- src/components/admin/ConfirmDeletePopup.jsx
+++ src/components/admin/ConfirmDeletePopup.jsx
@@ -1,18 +1,12 @@
 // ConfirmDeletePopup.jsx
 import React from "react";
-import { motion } from "framer-motion";
 
 const ConfirmDeletePopup = ({ isOpen, onConfirm, onCancel, itemType }) => {
 
   if (!isOpen) return null;
 
   return (
-    <motion.div
-      className="fixed inset-x-0 top-5 mx-auto bg-slate-100 shadow-lg rounded-lg p-4 w-1/4 z-50"
-      initial={{ opacity: 0, y: -100 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -100 }}
-      transition={{ duration: 0.3 }}
+    <div
     >
       <p className="text-gray-800 mb-4">
         Bạn có chắc chắn muốn xóa {itemType} này không?
@@ -31,7 +25,7 @@ const ConfirmDeletePopup = ({ isOpen, onConfirm, onCancel, itemType }) => {
           Xác nhận
         </button>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
